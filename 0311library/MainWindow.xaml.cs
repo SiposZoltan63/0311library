8bec0f35644381aa24a7756a4b636b718f799d45
SiposZoltan63 <siposz@kkszki.hu>
2026-03-11 08:44:39 +0100

    elso

---
+++ 0311library/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _0311library.megjelenit;
+
+namespace _0311library
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainPage.Navigate(new library(this));
+        }
+    }
+}
---
+++ 0311library/megjelenit/library.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _0311library
+{
+    /// <summary>
+    /// Interaction logic for library.xaml
+    /// </summary>
+    public partial class library : Window
+    {
+         = new Read();
+        CreateAuthors createAuthors = new CreateAuthors();
+        public library()
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+    }
+}
